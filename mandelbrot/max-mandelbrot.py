727be9d481d03a1b4e82a5ed434f74e7873bc686
Maxiboops <mpettica60@stuy.edu>
2026-03-23 22:11:01 -0400

    Refactor Mandelbrot code with Complex class

--- mandelbrot/max-mandelbrot.py
+++ mandelbrot/max-mandelbrot.py
@@ -1,5 +1,31 @@
 Web VPython 3.2
 
+#Make the class complex which stores all the numbers
+class Complex:
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+    
+    def add(self, c):
+        sum_real = self.real + c.real
+        sum_imaginary = self.imaginary + c.imaginary
+        return Complex(sum_real, sum_imaginary)
+    
+    def multiply(self, c):
+        q = self.real * c.real
+        w = self.real * c.imaginary
+        e = self.imaginary * c.real
+        r = self.imaginary * c.imaginary  * -1
+        
+        sum_real = q + r
+        sum_imaginary = w + e
+        return Complex(sum_real, sum_imaginary)
+    
+    def exponiate(self, n, current=0):
+        if n <= 0:
+            return current
+        return exponiate(n-1, current=self.multiply(self))
+
 
 black = color.black
 
@@ -10,51 +36,46 @@ mandelbrot_dots = gdots(graph=mandelbrot_graph, color=black)
 non_mandelbrot_dots = gdots(graph=mandelbrot_graph, color=color.red)
 
 #real max and min
-real_max = 2
-real_min = -2
+real_max = 1
+real_min = -2.5
 
 #complex-max and min
-imagine_max = 2
-imagine_min = -2
+imagine_max = 1
+imagine_min = -1
 
 #percision
-per = 0.2
+per = 0.010
+total_iterations = 100
+speed = False
 
-class Complex:
-    def __init__(self, real, complex):
-        self.real = real
-        self.complex = complex
-    def multiply(self, value):
-        #value must be complex
-        a = self.real * value.real
-        b = self.real * value.complex
-        c = self.complex * value.real
-        d = self.complex * value.complex * -1
-        
-        return Complex(a+d, b+c)
-        
 #find the next value in the mandelbrot set given a certain z and c value.
 def mandelbrot_next(z, c):
-    return z ** 2 + c
+    return c.add(z.exponiate(2))
 #recursive sequence to iterate through mandelbrot set, total amount of times
-def iterate_mandelbrot(z=0, c=0, max_iterations=0):
-    if z > 2:
-        return True 
+def iterate_mandelbrot(z, c, max_iterations=0):
+    if z.real > 2:
+        return (False, max_iterations)
     elif max_iterations <= 0:
-        return False
+        return (True, 0)
     
-    print(mandelbrot_next(z,c))
     return iterate_mandelbrot(mandelbrot_next(z,c), c, max_iterations - 1)
+
 
 for i in range(real_min, real_max, per):
     for j in range(imagine_min, imagine_max, per):
-        inital_c_val = i + j * sqrt(-1)
-        in_mandelbrot = iterate_mandelbrot(0, inital_c_val, 10)
+        inital_c_val = Complex(i, j)
+        in_mandelbrot = iterate_mandelbrot(Complex(0,0), inital_c_val, total_iterations)
         
-        if in_mandelbrot:
+        if in_mandelbrot[0]:
             mandelbrot_dots.plot(i, j)
         else:
-            non_mandelbrot_dots.plot(i,j)
-            
+            if not speed:
+                total = total_iterations - in_mandelbrot[1]
+                chosen_blue = 1 - total / total_iterations
+                chosen_color = vec(1, 0, chosen_blue)
+                set = gdots(graph=mandelbrot_graph, color=chosen_color)
+                set.plot(i,j)
+            else:
+                non_mandelbrot_dots.plot(i,j)
 
         
